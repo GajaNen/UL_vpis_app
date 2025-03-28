ef9b0492797660cfc5cdd4a22304bd85262932b6
GajaNen <33069162+GajaNen@users.noreply.github.com>
2025-03-28 21:06:40 +0100

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -3,5 +3,5 @@ rsconnect::setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
                token = Sys.getenv("TOKEN"),
                secret = Sys.getenv("SECRET"))
 # Deploy
-rsconnect::deployApp(appFiles = c("app.R", "data_final.csv", "js4checkbox.js", "www"),
+rsconnect::deployApp(appFiles = c("app.R", "data_final.csv", "js4checkbox.js", "www/UL_logo-RGB_barv.png"),
                     forceUpdate = T)
